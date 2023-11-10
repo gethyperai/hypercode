4183112bb26d12b0677adbf5b243336559d61122
Niloy Sikdar <niloysikdar30@gmail.com>
2023-11-10 12:11:48 +0530

    style(code): run lint:fix and pretty:fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  preset: "ts-jest/presets/default-esm", // Use the ESM preset for ts-jest
+  preset: 'ts-jest/presets/default-esm', // Use the ESM preset for ts-jest
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       useESM: true,
     },
   },
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  testEnvironment: "node", // Specify the test environment
-  testMatch: ["**/*.test.ts"], // Match test files
-  extensionsToTreatAsEsm: [".ts"], // Treat these extensions as ESM
+  testEnvironment: 'node', // Specify the test environment
+  testMatch: ['**/*.test.ts'], // Match test files
+  extensionsToTreatAsEsm: ['.ts'], // Treat these extensions as ESM
   // If you have a custom setup file, you need to use the full filename including extension
   transform: {}, // ts-jest will be used for ts/tsx files automatically
 };
