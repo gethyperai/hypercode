208a82073690993f456a62db425de57795807c8c
Niloy Sikdar <niloysikdar30@gmail.com>
2023-11-12 15:50:59 +0530

    build(package): modify config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   minify: true,
   treeshake: true,
