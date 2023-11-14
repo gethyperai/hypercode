74e621b1525826618aee2eb3c9139017178905dd
Niloy Sikdar <niloysikdar30@gmail.com>
2023-11-14 15:10:19 +0530

    feat(search): add `Search` class inside `Hyper`

---
+++ src/search/search.ts
@@ -0,0 +1,19 @@
+import { Hyper } from '../hyper';
+import { MethodParamsOptions, SearchResultData } from './@types';
+
+export class Search {
+  constructor(private readonly hyper: Hyper) {}
+
+  async execute(query: string, options?: MethodParamsOptions) {
+    const payload = options?.contextId
+      ? { query, context_id: options.contextId }
+      : { query };
+
+    const res = await this.hyper.post<SearchResultData[]>({
+      endpoint: '/search',
+      body: payload,
+    });
+
+    return res;
+  }
+}
