8aac1764c5e0c8d53c299f8727b3b0f90e809665
Niloy Sikdar <niloysikdar30@gmail.com>
2023-11-20 23:21:28 +0530

    docs(examples): add example for `with-nextjs-pages-router`

---
+++ examples/with-nextjs-pages-router/src/pages/index.tsx
@@ -0,0 +1,90 @@
+import Head from 'next/head';
+import { useState, type FormEvent } from 'react';
+
+const Home = () => {
+  const [query, setQuery] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<string>('');
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setQuery('');
+
+    try {
+      const res = await fetch('/api/hyper', {
+        method: 'POST',
+        body: JSON.stringify({ query }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const { data } = await res.json();
+
+      setResult(data);
+    } catch (error) {
+      console.error(error);
+      setResult('Error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Hypercode demo with Next.js pages router</title>
+      </Head>
+
+      <main className="min-h-screen flex justify-center items-center">
+        <div className="shadow-lg rounded-md px-6 py-4 border max-w-md mx-4">
+          <h1 className="text-2xl font-semibold">
+            Hypercode demo with Next.js pages router
+          </h1>
+
+          <p className="mt-4 text-lg">
+            Type a query in the search bar and press enter to get result of
+            "string" type
+          </p>
+
+          <form onSubmit={handleSubmit}>
+            <fieldset
+              disabled={isLoading}
+              className="flex flex-col justify-start gap-2 mt-6 group"
+            >
+              <label htmlFor="query">Query</label>
+
+              <input
+                type="text"
+                id="query"
+                name="query"
+                className="ring-2 ring-gray-600 border-gray-600 rounded-md px-2 py-1"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="bg-blue-500 text-white rounded-md w-fit px-6 py-1 mt-4"
+              >
+                Search
+              </button>
+            </fieldset>
+          </form>
+
+          {isLoading && <p className="mt-4">Loading...</p>}
+
+          {result && !isLoading && (
+            <div className="mt-4">
+              <p className="font-semibold">Result:</p>
+              <p>{result}</p>
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
